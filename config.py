eb7e21f7be8386f6e9e7a36887c5e86631e90606
Teemsoha <163581338+Teemsoha@users.noreply.github.com>
2024-05-06 05:42:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #AKI_MONGO_HOST = os.environ.get('', "")
-TOKEN = getenv("BOT_TOKEN", "")
+TOKEN = getenv("BOT_TOKEN", "7125295143:AAE1t8O67DVQ7mCjoqjI6t-MLE0CyJ_8dvI")
